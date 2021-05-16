1553e992a2648b3c2d09409569967d11e03a6b1b
Aaron Friel <mayreply@aaronfriel.com>
2021-05-15 22:59:00 -0700

    derived partialeq deref error?

--- tests/test.rs
+++ tests/test.rs
@@ -1,4 +1,3 @@
-
 use dyn_partial_eq::*;
 
 #[test]
@@ -29,4 +28,29 @@ fn test_dyn_partial_eq() {
   assert_eq!(&boxed_b == &boxed_a_zero, false);
   assert_eq!(&boxed_b == &boxed_a_one, false);
   assert_eq!(&boxed_b == &boxed_b, true);
+}
+
+#[test]
+fn test_dyn_nested_partial_eq() {
+  #[dyn_partial_eq]
+  trait ParentTrait {}
+
+  #[derive(PartialEq, DynPartialEq)]
+  struct Parent {
+    child: Box<dyn ChildTrait>,
+    /*
+ERROR:
+cannot move out of `*__self_1_0` which is behind a shared reference
+move occurs because `*__self_1_0` has type `Box<dyn ChildTrait>`, which does not implement the `Copy` trait
+rustc E0507
+    */
+  }
+  impl ParentTrait for Parent {}
+
+  #[dyn_partial_eq]
+  trait ChildTrait {}
+
+  #[derive(DynPartialEq, PartialEq)]
+  struct Child(usize);
+  impl ChildTrait for Child {}
 }
